82aa2b923b0e561ec2528703a923ab2b373971b9
JeffAndCherry <oncwnuNeICHVFFaaeuTkTxBP2L-4@git.weixin.qq.com>
2019-09-07 17:43:22 +0800

    删除多余的

--- wx-djm/cloudfunctions/getPosts/index.js
+++
@@ -1,9 +0,0 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-
-cloud.init()
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-  return { "statusCode": 200, "data": [{ "id": "1", "thumbnail": "/images/random.png", "category": "category", "date": "2019-1-12", "meta": { "thumbnail": "thumbnail", "views": "123" }, "title": { "rendered": "第一条消息的标题" } }, { "id": "2", "thumbnail": "/images/random.png", "category": "category", "date": "2019-1-12", "meta": { "thumbnail": "thumbnail", "views": "234" }, "title": { "rendered": "第二条消息的标题" } }, { "id": "3", "thumbnail": "/images/random.png", "category": "category", "date": "2019-1-12", "meta": { "thumbnail": "thumbnail", "views": "345" }, "title": { "rendered": "第三条消息的标题" } }]}
-}
